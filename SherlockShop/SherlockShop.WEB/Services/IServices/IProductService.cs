ebb2b8e1946e4f6a9822b4ae3b7316bdeeae95d8
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-08-02 15:36:02 +0300

    IServiceProduct and its associated Dto have been added to the WEB project. P.s. Code duplication when adding Dto is necessary in order to maintain the absolute independence of our third-party API and WEB project.

---
+++ SherlockShop/SherlockShop.WEB/Services/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using SherlockShop.WEB.Models;
+
+namespace SherlockShop.WEB.Services.IServices;
+
+public interface IProductService
+{
+	Task<T> GetAllProductsAsync<T>();
+	Task<T> GetProductByIdAsync<T>(int id);
+	Task<T> CreateProductAsync<T>(ProductDto productDto);
+	Task<T> UpdateProductAsync<T>(ProductDto productDto);
+	Task<T> DeleteProductAsync<T>(int id);
+}
