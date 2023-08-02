98ab2ec4f75c0d18702dfd657d29b9126b40d26f
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-08-02 16:33:25 +0300

    Added IBaseService and BaseService (to facilitate sending API requests) to the WEB project.

---
+++ SherlockShop/SherlockShop.WEB/Services/IServices/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using SherlockShop.WEB.Models;
+
+namespace SherlockShop.WEB.Services.IServices;
+
+public interface IBaseService: IDisposable
+{
+	ResponseDto responseModel { get; set; }
+	Task<T> SendAsync <T>(ApiRequest apiRequest);
+}
