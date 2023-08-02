1c746b54a619bb86324432e77c9d9bb827597f75
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-08-02 16:31:03 +0300

    Added ApiRequest model in WEB project.

---
+++ SherlockShop/SherlockShop.WEB/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static SherlockShop.WEB.SD;
+
+namespace SherlockShop.WEB.Models;
+
+public class ApiRequest
+{
+	public ApiType ApiType { get; set; } = ApiType.GET;
+	public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set; }
+}
