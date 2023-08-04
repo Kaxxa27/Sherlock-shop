9f387b2fb7047797f4ee52ac28698852b5aba8fd
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-08-04 15:37:47 +0300

    Added ProductCreate method in ProductController.

--- SherlockShop/SherlockShop.WEB/Controllers/ProductController.cs
+++ SherlockShop/SherlockShop.WEB/Controllers/ProductController.cs
@@ -2,25 +2,49 @@
 using Newtonsoft.Json;
 using SherlockShop.WEB.Models;
 using SherlockShop.WEB.Services.IServices;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SherlockShop.WEB.Controllers;
 
 public class ProductController : Controller
 {
-	private readonly IProductService _productService;
+    private readonly IProductService _productService;
 
     public ProductController(IProductService productService)
     {
         _productService = productService;
     }
+
+    [HttpGet]
     public async Task<IActionResult> ProductIndex()
-	{
+    {
         List<ProductDto>? list = new();
         var response = await _productService.GetAllProductsAsync<ResponseDto>();
 
-        if(response != null && response.IsSuccess)
+        if (response != null && response.IsSuccess)
             list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
 
-		return View(list);
-	}
+        return View(list);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ProductCreate()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ProductCreate(ProductDto model)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _productService.CreateProductAsync<ResponseDto>(model);
+
+            if (response != null && response.IsSuccess)
+                return RedirectToAction(nameof(ProductIndex));
+        }
+
+        return View(model);
+    }
 }
