a19299a0ddd8238c6a81847376c4de4c05902a90
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-08-25 13:44:35 +0300

    Added ApplicationDbContext and ApplicationUser in project Identity.

---
+++ SherlockShop/SherlockShop.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SherlockShop.Services.Identity.Models;
+
+namespace SherlockShop.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- SherlockShop/SherlockShop.Services.Identity/Program.cs
+++ SherlockShop/SherlockShop.Services.Identity/Program.cs
@@ -6,6 +6,10 @@ namespace SherlockShop.Services.Identity
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
